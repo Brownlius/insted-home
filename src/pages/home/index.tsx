00cd81274723e0e6ecefc03509273d4f9a6e2b3a
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2023-01-30 23:37:45 -0300

    .

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,39 @@
 import style from './home.module.scss'
+import { ReactComponent as Logo } from '../../assets/logo.svg'
 
 export default function Home() {
-    return(
-        <header className={style.header}>
-            <div className={style.header__logo}>
-            </div>
-            <nav className={style.header__nav}>
-                <ul>
-                    <li>Principal</li>
-                    <li>INSTED</li>
-                    <li>Graduação</li>
-                    <li>EAD</li>
-                    <li>Pós Graduação</li>
-                    <li>Outros cursos</li>
-                    <li>Contato</li>
-                </ul>
-            </nav>
-        </header>
-        )
+    const url = '../../assets/informativo_1.png';
+
+    return (
+        <>
+            <header className={style.header}>
+                <div className={style.header__logo}>
+                    <Logo />
+                </div>
+                <nav className={style.header__nav}>
+                    <ul>
+                        <li>Principal</li>
+                        <li>INSTED</li>
+                        <li>Graduação</li>
+                        <li>EAD</li>
+                        <li>Pós Graduação</li>
+                        <li>Outros cursos</li>
+                        <li>Contato</li>
+                    </ul>
+                </nav>
+            </header>
+            <main className={style.main} >
+                <div className={style.firstContainer} >
+                    <div className={style.firstContainer__avisos} >
+                        <img src={url} alt= "123" />
+                    </div>
+                </div>
+                <div className={style.secondContainer} >
+                    <div className={style.secondContainer__avisos} >
+
+                    </div>
+                </div>
+            </main>
+        </>
+    )
 }
