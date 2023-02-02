a1d279fa21a41ef54fe186012ec011e977d08515
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2023-02-02 13:29:34 -0400

    Swiper

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,15 @@
-import style from './home.module.scss'
-import { ReactComponent as Logo } from '../../assets/logo.svg'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import style from './home.module.scss';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import '/node_modules/swiper/swiper-bundle.css';
 import { Autoplay, Navigation, Pagination } from 'swiper';
 
 
 export default function Home() {
 
     return (
-        <main className={style.main} >
+        <main className={style.main}>
             <header className={style.header}>
                 <div className={style.header__logo}>
                     <Logo />
@@ -24,22 +26,34 @@ export default function Home() {
                     </ul>
                 </nav>
             </header>
-                <Swiper
-                    modules={[Navigation, Pagination, Autoplay]}
-                    className={style.container_avisos}
-                    loop={true}
-                    spaceBetween={5}
-                    autoplay={{ delay: 5000, disableOnInteraction: false, waitForTransition: true }}
-                    speed={600}
-                    slidesPerView={1}
-                >
-                    <SwiperSlide className={style.container_avisos__avisos} >
-                        <img src={require('../../assets/informativo_3.png')} alt="123" />
+            <div className={style.container_ultimos_avisos}>
+                <h2 className={style.container_ultimos_avisos__titulo}>Últimas notícias</h2>
+                <Swiper className={style.container_ultimos_avisos__avisos}>
+
+                    <SwiperSlide>
+                        <p>Novo site Insted</p>
                     </SwiperSlide>
-                    <SwiperSlide className={style.container_avisos__avisos} >
-                        <img src={require('../../assets/informativo_2.png')} alt="123" />
+                    <SwiperSlide>
+                        <p>Parangolé - 2009</p>
                     </SwiperSlide>
                 </Swiper>
-            </main>
+            </div>
+            <Swiper
+                modules={[Navigation, Pagination, Autoplay]}
+                className={style.container_avisos}
+                loop={true}
+                spaceBetween={5}
+                autoplay={{ delay: 5000, disableOnInteraction: false, waitForTransition: true }}
+                speed={600}
+                slidesPerView={1}
+            >
+                <SwiperSlide className={style.container_avisos__avisos} >
+                    <img src={require('../../assets/informativo_3.png')} alt="123" />
+                </SwiperSlide>
+                <SwiperSlide className={style.container_avisos__avisos} >
+                    <img src={require('../../assets/informativo_2.png')} alt="123" />
+                </SwiperSlide>
+            </Swiper>
+        </main>
     )
 }
