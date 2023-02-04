931656bb909eff89b59f3d13dc6c815f60d0b2fb
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2023-02-04 18:32:45 -0300

    .

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -38,22 +38,33 @@ export default function Home() {
                     </SwiperSlide>
                 </Swiper>
             </div>
-            <Swiper
-                modules={[Navigation, Pagination, Autoplay]}
-                className={style.container_avisos}
-                loop={true}
-                spaceBetween={5}
-                autoplay={{ delay: 5000, disableOnInteraction: false, waitForTransition: true }}
-                speed={600}
-                slidesPerView={1}
-            >
-                <SwiperSlide className={style.container_avisos__avisos} >
-                    <img src={require('../../assets/informativo_3.png')} alt="123" />
-                </SwiperSlide>
-                <SwiperSlide className={style.container_avisos__avisos} >
-                    <img src={require('../../assets/informativo_2.png')} alt="123" />
-                </SwiperSlide>
-            </Swiper>
-        </main>
+            <section className={style.principal}>
+                <Swiper
+                    modules={[Navigation, Pagination, Autoplay]}
+                    className={style.container_avisos}
+                    loop={true}
+                    spaceBetween={5}
+                    autoplay={{ delay: 5000, disableOnInteraction: false, waitForTransition: true }}
+                    speed={600}
+                    slidesPerView={1}
+                >
+                    <SwiperSlide className={style.container_avisos__avisos} >
+                        <img src={require('../../assets/informativo_1.png')} alt="123" />
+                    </SwiperSlide>
+                    <SwiperSlide className={style.container_avisos__avisos} >
+                        <img src={require('../../assets/informativo_3.png')} alt="123" />
+                    </SwiperSlide>
+                    <SwiperSlide className={style.container_avisos__avisos} >
+                        <img src={require('../../assets/informativo_2.png')} alt="123" />
+                    </SwiperSlide>
+                </Swiper>
+                <section
+                    className={style.container_avisos}
+                   
+                >
+                    
+                </section>
+            </section>
+        </main >
     )
 }
