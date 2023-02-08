e8e7cb594b4249bd056423e6ddbeb6705547e57f
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2023-02-07 23:31:46 -0300

    .

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -28,8 +28,13 @@ export default function Home() {
             </header>
             <div className={style.container_ultimos_avisos}>
                 <h2 className={style.container_ultimos_avisos__titulo}>Últimas notícias</h2>
-                <Swiper className={style.container_ultimos_avisos__avisos}>
-
+                <Swiper className={style.container_ultimos_avisos__avisos}
+                    modules={[Navigation, Pagination, Autoplay]}
+                    loop={true}
+                    spaceBetween={5}
+                    autoplay={{ delay: 5000, disableOnInteraction: false, waitForTransition: true }}
+                    speed={650}
+                    slidesPerView={1}>
                     <SwiperSlide>
                         <p>Novo site Insted</p>
                     </SwiperSlide>
@@ -44,7 +49,7 @@ export default function Home() {
                     className={style.container_avisos}
                     loop={true}
                     spaceBetween={5}
-                    autoplay={{ delay: 5000, disableOnInteraction: false, waitForTransition: true }}
+                    autoplay={{ delay: 6500, disableOnInteraction: false, waitForTransition: true }}
                     speed={600}
                     slidesPerView={1}
                 >
@@ -54,28 +59,40 @@ export default function Home() {
                     <SwiperSlide className={style.container_avisos__avisos} >
                         <img src={require('../../assets/informativo_3.png')} alt="123" />
                     </SwiperSlide>
-                    <SwiperSlide className={style.container_avisos__avisos} >
-                        <img src={require('../../assets/informativo_2.png')} alt="123" />
-                    </SwiperSlide>
+
                 </Swiper>
-                <section
-                    className={style.container_botoes}
-                >
-                    <div className={style.container_botoes__dois}>
-                        <img className={style.container_botoes__dois__botoes}
-                            src={require('../../assets/botoes/insted_botao-site_inscricao_DIPLOMA.png')} alt=" " />
-
-                        <img className={style.container_botoes__dois__botoes}
-                            src={require('../../assets/botoes/insted_botao-site_inscricao_NOTA-ENEM.png')} alt=" " />
+                <section className={style.botoes} >
+                    <div className={style.botoes__titulo}>
+                        <img src={require('../../assets/botoes/titulo.png')} alt=" " />
                     </div>
-                    <div className={style.container_botoes__dois}>
-                        <img className={style.container_botoes__dois__botoes}
-                            src={require('../../assets/botoes/insted_botao-site_inscricao_TRANSFERENCIA.png')} alt=" " />
+                    <div className={style.botoes__container}>
+                        <div className={style.botoes__container__dois}>
+                            <img className={style.botoes__container__dois__img}
+                                src={require('../../assets/botoes/diploma_botao.png')} alt=" " />
 
-                        <img className={style.container_botoes__dois__botoes}
-                            src={require('../../assets/botoes/insted_botao-site_inscricao-PROVA-ONLINE.png')} alt=" " />
+                            <img className={style.botoes__container__dois__img}
+                                src={require('../../assets/botoes/redacao_botao.png')} alt=" " />
+                        </div>
+                        <div className={style.botoes__container__dois}>
+                            <img className={style.botoes__container__dois__img}
+                                src={require('../../assets/botoes/transferencia_botao.png')} alt=" " />
+
+                            <img className={style.botoes__container__dois__img}
+                                src={require('../../assets/botoes/enem_botao.png')} alt=" " />
+                        </div>
                     </div>
                 </section>
+            </section>
+            <section className={style.nossos_cursos}>
+                <h2 className={style.nossos_cursos__titulo}>
+                    Conheça nossos cursos
+                </h2>
+                <p className={style.nossos_cursos__texto}>
+                    Quem busca destaque no mercado empresarial público e privado, quer ser Insted.
+                </p>
+                <p className={style.nossos_cursos__texto}>
+                    Aprenda a desenvolver todas as competências por meio de ferramentas e técnicas direcionadas.
+                </p>
             </section>
         </main >
     )
